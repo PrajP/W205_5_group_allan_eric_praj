3220a19dd459802e5cfb7e574809fd7c7414bdaf
allan-lo <allanlo78@gmail.com>
2016-11-02 16:59:12 -0700

    added -o option to output file name
    added -o option to output file name

--- Project-1/tweepy/collectTweets.py
+++ Project-1/tweepy/collectTweets.py
@@ -9,23 +9,29 @@ import datetime
 #add a parser for search strings
 parser = argparse.ArgumentParser(description='Parse the search strings.')
 parser.add_argument('-q', '--queryString', action="store", dest="queryString", metavar='QueryStrings', nargs='+', help='Enter the Search Query strings separated by space: -q Samsung galaxy iPhone...')
+parser.add_argument('-o', '--outputName', action="store", dest="output", metavar='outputFileName', nargs=1, help='Enter the Output File Name for the search')
 args = parser.parse_args()
-					
+				
 print "Search terms are: "+" ".join(args.queryString)
+print "Output file name is: " + args.output[0]
 
+#define search query string
 queryString_kw = " OR ".join(['"%s"' % s for s in args.queryString])
 queryString_hash = " OR ".join(['#%s' % s for s in args.queryString])
+#searchQuery = 'place:96683cc9126741d1 #samsung OR "samsung" OR "SamSung" OR "Galaxy" OR "galaxy phone"'
 
 searchQuery = 'place:96683cc9126741d1 ' + queryString_kw + " OR " + queryString_hash
 
-#searchQuery = 'place:96683cc9126741d1 #samsung OR "samsung" OR "SamSung" OR "Galaxy" OR "galaxy phone"'
-					
+#define output file name
+year_date = str(datetime.datetime.now()).split(' ')[0]
+fName = args.output[0]+'_SmartPhone_tweets_'+year_date+'.json' # We'll store the tweets in a text file.
+
+#access keys					
 consumer_key = 	'irBLC6hrDRi0Qjbd1OZb3Tvke'
 consumer_secret = 'oL3Ta2hJItV7BkZeoA9a1dtZWTMrW38aQ68IVdiQRqUlwOnt9u'
 access_token = '788174119955116033-GMbdhBRFXHrJHDj1UbkuC5fEVZ2J44W'
 access_secret = 'jL1q2eK8h63BhZKiAHlohQzF9E5ZeNXbHDVwkuyb9Fnlh'
 
-year_date = str(datetime.datetime.now()).split(' ')[0]
 
 if __name__ == "__main__": 
 	auth = OAuthHandler(consumer_key, consumer_secret)
@@ -38,8 +44,6 @@ if __name__ == "__main__":
 			  	
 	maxTweets = 10000000 # Some arbitrary large number
 	tweetsPerQry = 100  # this is the max the API permits
-	fName = 'SmartPhone_tweets_'+year_date+'.json' # We'll store the tweets in a text file.
-
 
 	# If results from a specific ID onwards are reqd, set since_id to that ID.
 	# else default to no lower limit, go as far back as API allows
