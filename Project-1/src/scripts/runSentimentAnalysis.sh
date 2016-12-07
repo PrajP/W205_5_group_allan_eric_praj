91a17601a7b89eca54205ecc7dbd88f394c43047
allan-lo <allanlo78@gmail.com>
2016-12-06 18:16:26 -0800

    git cleanup
    git cleanup

--- Project-1/src/scripts/runSentimentAnalysis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-LOGFILE=$1
-/usr/local/bin/python2.7 sentimentAnalysis.py > $LOGFILE 2>&1;
--- Project-1/src/scripts/sentimentAnalysis.py
+++ Project-1/src/scripts/sentimentAnalysis.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python2.7
 import numpy as np
 import pandas as pd
 from pandas.io.json import json_normalize
